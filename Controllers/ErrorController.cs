20993f1e200e175757eacb2e2dc8802b50f38f5f
Colm Sheehan <colms152@gmail.com>
2021-01-07 23:22:03 +0000

    passable

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eventchain.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Index(int code)
+        {
+            if (code == 404)
+            {
+                ViewBag.error = code;
+                return View("HttpError");
+            }
+            return View("HttpError");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -78,8 +78,9 @@ namespace Eventchain
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
+                app.UseExceptionHandler("/Home/Error");
+                //app.UseDeveloperExceptionPage();
+                // app.UseDatabaseErrorPage();
             }
             else
             {
@@ -87,6 +88,7 @@ namespace Eventchain
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseStatusCodePagesWithRedirects("/Error/Index?code={0}");
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
